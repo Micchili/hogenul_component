35a2cb19d9f2fdf90f730527cf3d0494bd859631
Miccili <wot.dannbo@gmail.com>
2019-02-19 13:14:05 +0900

    [FIX] 修正、とりあえず動く

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+function init(){
+    let push = document.getElementById("push");
+    let pacman = document.getElementById("pacman");
+    let i = ['#'];
 
-let push = document.getElementById("b");
-let pacman = document.getElementById("pacman");
-let i = ['#'];
+    push.addEventListener('click' ,waitInstall(), true);
 
-b.addEventListener('click' ,waitInstall(), true);
+    function waitInstall(){
+        console.log("押したよ");
+        setInterval(install(),2000);
+    }
 
-function waitInstall(){
-    setInterval(install(),500);
-}
-
-function install() {
+    function install() {
         for (let index = 0; index < 10; index++) {
             pacman.innerHTML = i;
             i.push('#');
+            console.log("可動したよ");
         }
+    }    
 }
+
+window.onload(init());
